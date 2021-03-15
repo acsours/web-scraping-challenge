b3ce5c3ca41465d431e95c603a8fae67e08185ac
Anna Sours <annasours@Annas-MacBook-Pro.local>
2021-03-15 14:04:52 -0600

    3 out of 4 websites scrape using flask

--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -1,6 +1,7 @@
 from splinter import Browser
 from bs4 import BeautifulSoup
 from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
 # import time
 
 def init_browser():
@@ -31,6 +32,49 @@ def scrape():
 
     mars_data['news_title']=news_title
     mars_data['news_p']=news_p
+
+
+    # Mars Facts tables scraping using pandas
+    tables = pd.read_html(url_3)
+    mars_facts_df=tables[0]
+    mars_facts_df.columns=['Metric', 'measurement']
+    mars_facts_df=mars_facts_df.set_index('Metric')
+    html_table = mars_facts_df.to_html()
+
+    mars_data['table']=html_table
+
+    # Mars Hemispheres objects
+    browser.visit(url_4)
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
+    photos = soup.find_all('div', class_='description')
+    base_url='https://astrogeology.usgs.gov'
+    img_dict_list=[]
+
+# browser.visit(url_4)
+
+    for each_photo in photos:
+    #     find img_url
+        h3 = each_photo.find('h3')
+        link = each_photo.find('a')
+        href = link['href']
+        title=h3.text
+
+        # print(f'Getting {title} link')
+
+    #     this will click on each photo link.
+        browser.links.find_by_partial_text(title).click()
+        pic_html = browser.html
+        pic_soup = BeautifulSoup(pic_html, 'html.parser')
+        source_photo=pic_soup.find('img', class_="wide-image")['src']
+        img_url=(base_url+source_photo)
+
+    #     create dictionary with title and img url and append to list of dictionaries
+        img_dict_list.append({'title': title, 'img_url': img_url})
+        
+    #     go back to original page to follow next link
+        browser.visit(url_4)
+    mars_data['hemispheres']=img_dict_list
 
     browser.quit()
 
